e7d9837dc788aa3aab573948346bcd90eb1ddfc7
zhengxingtao <zheng_xigntao@126.com>
2018-09-05 09:26:21 +0800

    运单dto

---
+++ tms-api/src/main/java/com/jaagro/tms/api/dto/waybill/ShowTrackingDto.java
@@ -0,0 +1,64 @@
+package com.jaagro.tms.api.dto.waybill;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author tony
+ */
+@Data
+@Accessors(chain = true)
+public class ShowTrackingDto implements Serializable {
+    /**
+     *
+     */
+    private Integer id;
+
+    /**
+     *
+     */
+    private Integer waybillId;
+
+    /**
+     * 修改前状态
+     */
+    private String oldStatus;
+
+    /**
+     * 修改后状态
+     */
+    private String newStatus;
+
+    /**
+     * 司机id：若是APP操作修改，填写此处
+     */
+    private Integer driverId;
+
+    /**
+     * 纬度
+     */
+    private BigDecimal latitude;
+
+    /**
+     * 经度
+     */
+    private BigDecimal longitude;
+
+    /**
+     * 轨迹描述
+     */
+    private String trackingInfo;
+
+    /**
+     * 发起修改设备序列号
+     */
+    private String device;
+
+    /**
+     * 修改人id：若是后台人员修改则填写此处
+     */
+    private Integer referUserId;
+}
