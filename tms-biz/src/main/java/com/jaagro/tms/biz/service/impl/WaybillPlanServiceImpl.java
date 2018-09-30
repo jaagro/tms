a65aeb99868e50b51d7516cdc0a875891d5ae09e
zhengxingtao <zheng_xigntao@126.com>
2018-09-30 10:54:55 +0800

    更新环境变量

--- tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillPlanServiceImpl.java
+++ tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillPlanServiceImpl.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
