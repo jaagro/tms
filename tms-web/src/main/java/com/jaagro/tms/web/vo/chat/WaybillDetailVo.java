3ad68ddf87b2992418e670e44e4ce326cff8199a
wanghq <gavinwang0224@aliyun.com>
2018-10-30 14:50:32 +0800

    微信小程序运单详情

--- tms-web/src/main/java/com/jaagro/tms/web/vo/chat/WaybillDetailVo.java
+++ tms-web/src/main/java/com/jaagro/tms/web/vo/chat/WaybillDetailVo.java
@@ -1,4 +1,60 @@
 package com.jaagro.tms.web.vo.chat;
 
+import com.jaagro.tms.api.dto.truck.ShowDriverDto;
+import com.jaagro.tms.api.dto.truck.ShowTruckDto;
+import com.jaagro.tms.api.dto.waybill.GetTrackingDto;
+import com.jaagro.tms.api.dto.waybill.GetWaybillItemDto;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Author gavin
+ * 微信小程序运单详情vo
+ */
+@Data
+@Accessors(chain = true)
 public class WaybillDetailVo {
+
+    /**
+     *
+     */
+    private Integer id;
+    /**
+     * 装货时间
+     */
+    private Date loadTime;
+    /**
+     * 装货地
+     */
+    private ShowSiteVo showSiteVo;
+
+    /**
+     * 运单指派的车辆详情
+     */
+    private ShowTruckDto assginedTruckDto;
+
+    /**
+     * 司机详情
+     */
+    private ShowDriverDto driverDto;
+
+
+    /**
+     * 运单明细list
+     */
+    private List<GetWaybillItemDto> waybillItems;
+
+    /**
+     * 运单轨迹
+     */
+    private List<GetTrackingDto> tracking;
+
+    /**
+     * 货物类型
+     */
+    private Integer goodType;
+
 }
