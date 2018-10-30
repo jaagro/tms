24f65810dbd771f07adb3670220f833f98c7ed88
baiyiran <1169172027@qq.com>
2018-10-30 14:42:16 +0800

    订单详情bug修复

--- tms-web/src/main/java/com/jaagro/tms/web/controller/WeChatAppletOrderController.java
+++ tms-web/src/main/java/com/jaagro/tms/web/controller/WeChatAppletOrderController.java
@@ -1,11 +1,15 @@
 package com.jaagro.tms.web.controller;
 
+import com.jaagro.tms.api.dto.order.GetOrderDto;
 import com.jaagro.tms.api.dto.order.ListOrderCriteriaDto;
+import com.jaagro.tms.api.service.OrderRefactorService;
 import com.jaagro.tms.api.service.OrderService;
+import com.jaagro.tms.web.vo.order.*;
 import com.jaagro.utils.BaseResponse;
 import com.jaagro.utils.ResponseStatusCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
@@ -20,6 +24,8 @@ public class WeChatAppletOrderController {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private OrderRefactorService orderRefactorService;
 
     /**
      * 查询单条订单
@@ -28,9 +34,47 @@ public class WeChatAppletOrderController {
      * @return
      */
     @ApiOperation("查询单条订单")
-    @GetMapping("/getWechatOrderById/{id}")
+    @GetMapping("/getWeChatOrderById/{id}")
     public BaseResponse getOrderById(@PathVariable("id") Integer id) {
-        return BaseResponse.successInstance(orderService.getOrderById(id));
+        OrderVo orderVo = new OrderVo();
+        try {
+            GetOrderDto getOrderDto = orderRefactorService.getOrderById(id);
+            if (getOrderDto != null) {
+                BeanUtils.copyProperties(getOrderDto, orderVo);
+                //联系人
+                CustomerContactsVo contactsVo = new CustomerContactsVo();
+                BeanUtils.copyProperties(getOrderDto.getContactsDto(), contactsVo);
+                orderVo.setContactsDto(contactsVo);
+                //创建人
+                UserVo userVo = new UserVo();
+                BeanUtils.copyProperties(getOrderDto.getCreatedUser(), userVo);
+                orderVo.setCreatedUser(userVo);
+                //客户
+                CustomerVo customerVo = new CustomerVo();
+                BeanUtils.copyProperties(getOrderDto.getCustomer(), customerVo);
+                orderVo.setCustomer(customerVo);
+                //装货地
+                SiteVo siteVo = new SiteVo();
+                BeanUtils.copyProperties(getOrderDto.getLoadSiteId(), siteVo);
+                orderVo.setLoadSiteId(siteVo);
+                //修改人
+                if (getOrderDto.getModifyUser() != null) {
+                    UserVo userModifyVo = new UserVo();
+                    BeanUtils.copyProperties(getOrderDto.getModifyUser(), userModifyVo);
+                    orderVo.setModifyUser(userModifyVo);
+                }
+                //客户合同
+                ShowCustomerContractVo showCustomerContractVo = new ShowCustomerContractVo();
+                BeanUtils.copyProperties(getOrderDto.getCustomerContract(), showCustomerContractVo);
+                orderVo.setCustomerContract(showCustomerContractVo);
+                //订单详情
+                BeanUtils.copyProperties(getOrderDto.getOrderItems(), orderVo.getOrderItems());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponse.errorInstance(ResponseStatusCode.QUERY_DATA_ERROR.getCode(), ex.getMessage());
+        }
+        return BaseResponse.successInstance(orderVo);
     }
 
     /**
@@ -40,7 +84,7 @@ public class WeChatAppletOrderController {
      * @return
      */
     @ApiOperation("分页查询订单")
-    @PostMapping("/listWechatOrders")
+    @PostMapping("/listWeChatOrders")
     public BaseResponse listOrders(@RequestBody ListOrderCriteriaDto criteriaDto) {
         if (StringUtils.isEmpty(criteriaDto.getPageNum())) {
             return BaseResponse.errorInstance(ResponseStatusCode.QUERY_DATA_ERROR.getCode(), "pageNum不能为空");
@@ -59,7 +103,7 @@ public class WeChatAppletOrderController {
      * @return
      */
     @ApiOperation("取消订单")
-    @PostMapping("/cancelWechatOrder/{orderId}/{detailInfo}")
+    @PostMapping("/cancelWeChatOrder/{orderId}/{detailInfo}")
     public BaseResponse cancelOrders(@PathVariable("orderId") Integer orderId, @PathVariable("detailInfo") String detailInfo) {
         if (StringUtils.isEmpty(orderId)) {
             return BaseResponse.idNull("订单id不能为空");
---
+++ tms-web/src/main/java/com/jaagro/tms/web/vo/chat/WeChatOrderVo.java
@@ -0,0 +1,59 @@
+package com.jaagro.tms.web.vo.chat;
+
+import com.jaagro.tms.web.vo.order.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author baiyiran
+ * @Date 2018/10/30
+ */
+@Data
+@Accessors(chain = true)
+public class WeChatOrderVo implements Serializable {
+
+    /**
+     *
+     */
+    private Integer id;
+
+    /**
+     * 货物类型
+     */
+    private Integer goodsType;
+
+    /**
+     * 订单状态:
+     */
+    private String orderStatus;
+
+    /**
+     * 客户id
+     */
+    private CustomerVo customer;
+
+    /**
+     * 装货地id
+     */
+    private SiteVo loadSiteId;
+
+    /**
+     * 要求装货时间
+     */
+    private Date loadTime;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 订单列表
+     */
+    private List<OrderItemsVo> orderItems;
+
+}
