197a6e6a761b7973af8cebf66593709da68b2c0e
gaoxin <1506645910@qq.com>
2018-10-31 14:47:51 +0800

    app 异常上报1

--- tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillAnomalyServiceImpl.java
+++ tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillAnomalyServiceImpl.java
@@ -5,12 +5,15 @@ import com.jaagro.tms.api.dto.anomaly.WaybillAbnomalTypeDto;
 import com.jaagro.tms.api.dto.anomaly.WaybillAnomalyReportDto;
 import com.jaagro.tms.api.service.WaybillAnomalyService;
 import com.jaagro.tms.biz.entity.WaybillAnomaly;
+import com.jaagro.tms.biz.entity.WaybillAnomalyImage;
 import com.jaagro.tms.biz.entity.WaybillAnomalyType;
+import com.jaagro.tms.biz.mapper.WaybillAnomalyImageMapperExt;
 import com.jaagro.tms.biz.mapper.WaybillAnomalyMapperExt;
 import com.jaagro.tms.biz.mapper.WaybillAnomalyTypeMapperExt;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,6 +28,8 @@ public class WaybillAnomalyServiceImpl implements WaybillAnomalyService {
     @Autowired
     private WaybillAnomalyTypeMapperExt waybillAnomalyTypeMapper;
     @Autowired
+    private WaybillAnomalyImageMapperExt waybillAnomalyImageMapper;
+    @Autowired
     private CurrentUserService currentUserService;
 
     /**
@@ -33,12 +38,22 @@ public class WaybillAnomalyServiceImpl implements WaybillAnomalyService {
      * @return
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void waybillAnomalyReport(WaybillAnomalyReportDto dto) {
+        //插入异常表
         WaybillAnomaly waybillAnomaly = new WaybillAnomaly();
         BeanUtils.copyProperties(dto,waybillAnomaly);
         UserInfo currentUser = currentUserService.getCurrentUser();
         waybillAnomaly.setCreateUserId(currentUser.getId());
         waybillAnomalyMapper.insertSelective(waybillAnomaly);
+        //插入异常图片表
+        WaybillAnomalyImage waybillAnomalyImage = new WaybillAnomalyImage();
+        waybillAnomalyImage
+                .setAnomalyId(waybillAnomaly.getId())
+                .setCreateUserId(currentUser.getId())
+                .setWaybillId(dto.getWaybillId())
+                .setImageUrl(dto.getImageUrl());
+        waybillAnomalyImageMapper.insertSelective(waybillAnomalyImage);
     }
 
     /**
