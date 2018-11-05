07040e9acf071c1debbec14a7bc054e8d6752c63
wanghq <gavinwang0224@aliyun.com>
2018-11-05 11:06:42 +0800

    定位

--- tms-web/src/main/java/com/jaagro/tms/web/TmsApplication.java
+++ tms-web/src/main/java/com/jaagro/tms/web/TmsApplication.java
@@ -3,6 +3,7 @@ package com.jaagro.tms.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
