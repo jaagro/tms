67393591d593560c513cbce9ae593a3e33ee1031
zhengxingtao <zheng_xigntao@126.com>
2018-11-08 17:22:31 +0800

    异常状态常量

---
+++ tms-api/src/main/java/com/jaagro/tms/api/constant/AnormalyStatus.java
@@ -0,0 +1,17 @@
+package com.jaagro.tms.api.constant;
+
+/**
+ * 异常状态
+ */
+public final class AnormalyStatus {
+
+    public final static String TO_DO = "待处理";
+
+    public final static String DONE = "已处理";
+
+    public final static String TO_AUDIT = "待审核";
+
+    public final static String FINISH = "已结束";
+
+    public final static String INVALID = "已作废";
+}
