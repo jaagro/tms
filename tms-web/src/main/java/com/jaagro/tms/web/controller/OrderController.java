157e7d2afeb6345fd759b3bac5fb2f1b06d3183e
baiyiran <1169172027@qq.com>
2018-11-26 17:34:05 +0800

    订单管理和派单管理bug 修复

--- tms-web/src/main/java/com/jaagro/tms/web/controller/OrderController.java
+++ tms-web/src/main/java/com/jaagro/tms/web/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.PageInfo;
 import com.jaagro.constant.UserInfo;
 import com.jaagro.tms.api.constant.OrderStatus;
 import com.jaagro.tms.api.dto.base.ShowUserDto;
-import com.jaagro.tms.api.dto.customer.ShowSiteDto;
 import com.jaagro.tms.api.dto.order.*;
 import com.jaagro.tms.api.dto.waybill.ListWaybillDto;
 import com.jaagro.tms.api.service.OrderRefactorService;
@@ -242,14 +241,8 @@ public class OrderController {
                 orderVo
                         .setCustomerId(customerService.getShowCustomerById(orderDto.getCustomerId()))
                         .setCustomerContract(customerService.getShowCustomerContractById(orderDto.getCustomerContractId()))
-                        .setLoadSite(customerService.getShowSiteById(orderDto.getLoadSiteId()));
-                //归属网点名称
-                ShowSiteDto showSiteDto = customerService.getShowSiteById(orderDto.getLoadSiteId());
-                if (showSiteDto != null) {
-                    if (!StringUtils.isEmpty(showSiteDto.getDeptId())) {
-                        orderVo.setDepartmentName(userClientService.getDeptNameById(showSiteDto.getDeptId()));
-                    }
-                }
+                        .setLoadSite(customerService.getShowSiteById(orderDto.getLoadSiteId()))
+                        .setDepartmentName(userClientService.getDeptNameById(orderDto.getDepartmentId()));
                 //创单人
                 UserInfo userInfo = authClientService.getUserInfoById(orderDto.getCreatedUserId(), "employee");
                 if (userInfo != null) {
@@ -356,14 +349,8 @@ public class OrderController {
                 orderVo
                         .setCustomerId(customerService.getShowCustomerById(orderDto.getCustomerId()))
                         .setCustomerContract(customerService.getShowCustomerContractById(orderDto.getCustomerContractId()))
-                        .setLoadSite(customerService.getShowSiteById(orderDto.getLoadSiteId()));
-                //归属网点名称
-                ShowSiteDto showSiteDto = customerService.getShowSiteById(orderDto.getLoadSiteId());
-                if (showSiteDto != null) {
-                    if (!StringUtils.isEmpty(showSiteDto.getDeptId())) {
-                        orderVo.setDepartmentName(userClientService.getDeptNameById(showSiteDto.getDeptId()));
-                    }
-                }
+                        .setLoadSite(customerService.getShowSiteById(orderDto.getLoadSiteId()))
+                        .setDepartmentName(userClientService.getDeptNameById(orderDto.getDepartmentId()));
                 //创单人
                 UserInfo userInfo = authClientService.getUserInfoById(orderDto.getCreatedUserId(), "employee");
                 if (userInfo != null) {
