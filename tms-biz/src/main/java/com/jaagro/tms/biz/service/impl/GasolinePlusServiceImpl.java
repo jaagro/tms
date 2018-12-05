44ffc5b798c018935511736ec70ce73429b605fd
gaoxin <1506645910@qq.com>
2018-12-05 10:26:55 +0800

    我的驾照功能开发

---
+++ tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/GasolinePlusServiceImpl.java
@@ -0,0 +1,11 @@
+package com.jaagro.tms.biz.service.impl;
+
+import com.jaagro.tms.api.service.GasolinePlusService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author @Gao.
+ */
+@Service
+public class GasolinePlusServiceImpl implements GasolinePlusService {
+}
