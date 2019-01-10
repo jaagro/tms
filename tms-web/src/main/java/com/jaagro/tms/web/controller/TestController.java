30b2a911ee1223f959593f7032ef95517edc98a2
wanghq <gavinwang0224@aliyun.com>
2019-01-10 11:37:44 +0800

    导入运单后端接口测试

--- tms-web/src/main/java/com/jaagro/tms/web/controller/TestController.java
+++ tms-web/src/main/java/com/jaagro/tms/web/controller/TestController.java
@@ -1,17 +1,19 @@
 package com.jaagro.tms.web.controller;
 
+import com.jaagro.tms.api.dto.waybill.ImportWaybillDto;
 import com.jaagro.tms.api.dto.waybill.LocationDto;
+import com.jaagro.tms.api.service.WaybillService;
 import com.jaagro.tms.biz.mapper.LocationMapperExt;
-import com.jaagro.tms.biz.service.CustomerClientService;
 import com.jaagro.tms.biz.service.impl.CurrentUserService;
 import com.jaagro.tms.biz.service.impl.GpsLocationAsync;
-import com.jaagro.utils.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,10 +31,54 @@ public class TestController {
     @Autowired
     LocationMapperExt locationMapper;
 
+    @Autowired
+    private WaybillService waybillService;
+
 
     @Autowired
     private GpsLocationAsync asyncTask;
 
+    @GetMapping("/importWaybillsForChilken")
+    public void importChilkenWaybill() {
+        List<ImportWaybillDto> importDtos = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String loadtime1 = "2019-01-11 02:55:00";
+        String requiredtime1 = "2019-01-11 04:40:00";
+        try {
+            ImportWaybillDto importDto1 = new ImportWaybillDto();
+            importDto1.setOrderId(935);
+            importDto1.setCustomerId(359);
+            importDto1.setGoodsQuantity(324);
+            importDto1.setLoadSiteId(3916);
+            importDto1.setLoadTime(sdf.parse(loadtime1));
+            importDto1.setLoadSiteDeptId(156);
+            importDto1.setRequiredTime(sdf.parse(requiredtime1));
+            importDto1.setTruckId(459);
+            importDto1.setTruckNumber("8899");
+            importDto1.setTruckTypeId(8);
+            importDto1.setTruckTeamContractId(11);
+
+            String loadtime2 = "2019-01-11 05:30:00";
+            String requiredtime2 = "2019-01-11 07:15:00\n";
+            ImportWaybillDto importDto2 = new ImportWaybillDto();
+            importDto2.setOrderId(935);
+            importDto2.setCustomerId(359);
+            importDto2.setGoodsQuantity(324);
+            importDto2.setLoadSiteId(3916);
+            importDto2.setLoadTime(sdf.parse(loadtime2));
+            importDto2.setLoadSiteDeptId(156);
+            importDto2.setRequiredTime(sdf.parse(requiredtime2));
+            importDto2.setTruckId(459);
+            importDto2.setTruckNumber("8899");
+            importDto2.setTruckTypeId(8);
+            importDto2.setTruckTeamContractId(11);
+            importDtos.add(importDto2);
+
+            waybillService.importWaybills(935, importDtos);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     @GetMapping("/insertBatch")
     public void insertBatch() {
