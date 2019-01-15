4ceb0aac95b930b89edff8ea4800b48082088906
yogasan <yoga330356233@126.com>
2019-01-15 15:20:39 +0800

    毛鸡导入解析

--- tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillServiceImpl.java
+++ tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/WaybillServiceImpl.java
@@ -30,11 +30,14 @@ import com.jaagro.tms.biz.utils.RedisLock;
 import com.jaagro.utils.BaseResponse;
 import com.jaagro.utils.ResponseStatusCode;
 import com.jaagro.utils.ServiceResult;
+import com.netflix.discovery.converters.Auto;
 import org.apache.commons.lang.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -62,6 +65,7 @@ public class WaybillServiceImpl implements WaybillService {
      * 毛鸡导入屠宰日期所在列数
      */
     private static final int TRANSPORT_DAY_INDEX = 19;
+    private static final String CHICKEN_IMPORT = "chicken_import_";
 
     @Autowired
     private CurrentUserService currentUserService;
@@ -113,6 +117,8 @@ public class WaybillServiceImpl implements WaybillService {
     private MessageService messageService;
     @Autowired
     private ChickenImportRecordMapperExt chickenImportRecordMapperExt;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 毛鸡运单导入
@@ -269,6 +275,40 @@ public class WaybillServiceImpl implements WaybillService {
     @Override
     public Integer countUnFinishWaybillByDriver(Integer driverId) {
         return waybillMapper.countUnDoneByDriverId(driverId);
+    }
+
+    /**
+     * 修改不正确的毛鸡导入记录
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+    public List<ChickenImportRecordDto> changeImportChickenRecord(UpdateChickenImportRecordDto dto) {
+        Orders orders = ordersMapper.selectByPrimaryKey(dto.getOrderId());
+        HashOperations<String, Object, Object> opsForHash = redisTemplate.opsForHash();
+        String key = CHICKEN_IMPORT + dto.getOrderId();
+        Object object = opsForHash.get(key, dto.getSerialNumber());
+        if (object != null){
+           ChickenImportRecordDto chickenImportRecordDto = (ChickenImportRecordDto) object;
+            BaseResponse<GetTruckDto> res = truckClientService.getByTruckNumber(dto.getTruckNumber());
+            GetTruckDto truckDto = res.getData();
+            if (res != null && truckDto != null) {
+                ListTruckTypeDto truckTypeDto = truckDto.getTruckTypeId();
+                boolean checkTruckType = truckTypeDto != null && ProductName.CHICKEN.equals(truckTypeDto.getProductName()) && (chickenImportRecordDto.getGoodsQuantity() != null && chickenImportRecordDto.getGoodsQuantity().equals(truckTypeDto.getTruckAmount()));
+                if (checkTruckType) {
+                    chickenImportRecordDto.setVerifyPass(true);
+                }
+                chickenImportRecordDto.setTruckId(truckDto.getId());
+                chickenImportRecordDto.setTruckTypeId(truckTypeDto == null ? null : truckTypeDto.getId());
+                chickenImportRecordDto.setTruckTypeName(truckTypeDto == null ? null : truckTypeDto.getTypeName());
+                // 获取车队合同id
+                chickenImportRecordDto.setTruckTeamContractId(getTruckTeamContractId(orders.getGoodsType(), truckDto.getTruckTeamId()));
+                opsForHash.put(key,dto.getSerialNumber(),dto);
+            }
+        }
+        Map<Object, Object> entries = opsForHash.entries(key);
+        return getChickenImportRecordDtoListFromMap(entries);
     }
 
     /**
@@ -2199,6 +2239,8 @@ public class WaybillServiceImpl implements WaybillService {
             log.info("uploadUrl={},excelContent={}", preImportChickenRecordDto.getUploadUrl(), JSON.toJSONString(lists.get(0)));
             // 将excel内容解析为dto
             List<ChickenImportRecordDto> chickenImportRecordDtoList = parsingExcel(lists.get(0), preImportChickenRecordDto);
+            // 数据存入redis 批量插入存入hash保证原子性
+            putChickenImportRecordToRedis(chickenImportRecordDtoList);
             return chickenImportRecordDtoList;
         } catch (Exception e) {
             log.error("preImportChickenWaybill error preImportChickenRecordDto=" + preImportChickenRecordDto, e);
@@ -2209,13 +2251,16 @@ public class WaybillServiceImpl implements WaybillService {
     /**
      * 毛鸡导入记录入库并生成运单派单给车辆下所有司机
      *
-     * @param chickenImportRecordDtoValidList
+     * @param orderId
      */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public void importChickenWaybill(ValidList<ChickenImportRecordDto> chickenImportRecordDtoValidList) {
+    public void importChickenWaybill(Integer orderId) {
+        String key = CHICKEN_IMPORT+orderId;
+        HashOperations<String, Object, Object> opsForHash = redisTemplate.opsForHash();
+        Map<Object, Object> entries = opsForHash.entries(key);
+        List<ChickenImportRecordDto> chickenImportRecordDtoValidList = getChickenImportRecordDtoListFromMap(entries);
         if (!CollectionUtils.isEmpty(chickenImportRecordDtoValidList)) {
-            Integer orderId = chickenImportRecordDtoValidList.get(0).getOrderId();
             // 判断运单状态,只有已下单的运单才能做毛鸡导入
             judgeOrderForChickenImport(orderId);
             List<ChickenImportRecord> chickenImportRecordList = new ArrayList<>();
@@ -2241,6 +2286,10 @@ public class WaybillServiceImpl implements WaybillService {
             chickenImportRecordMapperExt.batchInsert(chickenImportRecordList);
             // 生成运单并派给车辆,如果表格数量过多需要起任务处理,防止处理时间过长事务不释放一直占用数据库链接
             importWaybills(orderId, importWaybillDtoList);
+            // 清空缓存
+            redisTemplate.delete(key);
+        }else {
+            throw new RuntimeException("导入失败");
         }
     }
 
@@ -2340,7 +2389,8 @@ public class WaybillServiceImpl implements WaybillService {
                         .setLoadSiteId(preImportChickenRecordDto.getLoadSiteId())
                         .setLoadSiteName(preImportChickenRecordDto.getLoadSiteName())
                         .setOrderId(preImportChickenRecordDto.getOrderId())
-                        .setVerifyPass(false);
+                        .setVerifyPass(false)
+                        .setSerialNumber(StringUtils.isEmpty(cells[1]) ? null : Integer.parseInt(cells[1]));
                 // 装货时间(车入鸡场时间)
                 Date loadTime = sdf.parse(day + " " + cells[10]);
                 dto.setLoadTime(loadTime);
@@ -2412,5 +2462,24 @@ public class WaybillServiceImpl implements WaybillService {
         if (!GoodsType.CHICKEN.equals(orders.getGoodsType())) {
             throw new RuntimeException("只有毛鸡订单才可以导入");
         }
+    }
+
+    private void putChickenImportRecordToRedis(List<ChickenImportRecordDto> chickenImportRecordDtoList) {
+        if (!CollectionUtils.isEmpty(chickenImportRecordDtoList)) {
+            HashOperations<String, Object, Object> opsForHash = redisTemplate.opsForHash();
+            Integer orderId = chickenImportRecordDtoList.get(0).getOrderId();
+            String key = CHICKEN_IMPORT+orderId;
+            Map<Integer,ChickenImportRecordDto> map = new LinkedHashMap<>();
+            chickenImportRecordDtoList.forEach(dto->map.put(dto.getSerialNumber(),dto));
+            opsForHash.putAll(key,map);
+        }
+    }
+
+    private List<ChickenImportRecordDto> getChickenImportRecordDtoListFromMap(Map<Object,Object> map){
+        List<ChickenImportRecordDto> chickenImportRecordDtoList = new ArrayList<>();
+        Set<Object> ketSet = map.keySet();
+        Iterator<Object> iterator = ketSet.iterator();
+        iterator.forEachRemaining(element->chickenImportRecordDtoList.add((ChickenImportRecordDto)map.get(element)));
+        return chickenImportRecordDtoList;
     }
 }
