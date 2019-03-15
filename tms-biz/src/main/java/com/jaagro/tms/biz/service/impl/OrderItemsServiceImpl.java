7ef956d4e88feca34598580ae43a3c124afda940
baiyiran <1169172027@qq.com>
2019-03-15 13:59:02 +0800

    123

--- tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/OrderItemsServiceImpl.java
+++ tms-biz/src/main/java/com/jaagro/tms/biz/service/impl/OrderItemsServiceImpl.java
@@ -64,9 +64,9 @@ public class OrderItemsServiceImpl implements OrderItemsService {
             for (CreateOrderGoodsDto goodsDto : orderItemDto.getGoods()
             ) {
                 if (GoodsType.FODDER.equals(order.getGoodsType())) {
-                    if (StringUtils.isEmpty(goodsDto.getFeedType())) {
-                        throw new NullPointerException("饲料类型不能为空");
-                    }
+//                    if (StringUtils.isEmpty(goodsDto.getFeedType())) {
+//                        throw new NullPointerException("饲料类型不能为空");
+//                    }
                 }
                 goodsDto
                         .setId(null)
